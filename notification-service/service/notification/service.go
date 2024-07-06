7ce13044490e6c95d5b7f0ae35035f2b56b4e7d0
majid <majidmohsenifar@gmail.com>
2024-07-06 18:18:10 +0330

    tests added for consumer

--- notification-service/service/notification/service.go
+++ notification-service/service/notification/service.go
@@ -1,8 +1,13 @@
 package notification
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 type Service struct {
+	allNotifications []SendNotificationParams
+	mutex            sync.Mutex
 }
 
 type SendNotificationParams struct {
@@ -15,7 +20,21 @@ type SendNotificationParams struct {
 }
 
 func (s *Service) SendNotification(ctx context.Context, params SendNotificationParams) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if len(s.allNotifications) > 10 {
+		//empty slices without re allocation
+		s.allNotifications = s.allNotifications[:0]
+	}
+	s.allNotifications = append(s.allNotifications, params)
 	return nil
+}
+
+// just for test purposes
+func (s *Service) GetAllNotifications() []SendNotificationParams {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.allNotifications
 }
 
 func NewService() *Service {
--- notification-service/test/transaction_created_consumer_test.go
+++ notification-service/test/transaction_created_consumer_test.go
@@ -1,9 +1,78 @@
 package test
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	transactionpb "github.com/majidmohsenifar/heli-tech/data-contracts/proto/transaction"
+	"github.com/majidmohsenifar/heli-tech/notification-service/core"
+	"github.com/majidmohsenifar/heli-tech/notification-service/handler/consumer"
+	"github.com/majidmohsenifar/heli-tech/notification-service/service/notification"
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 )
 
-func TestTransactionCreatedConsumer(t *testing.T) {
+func Test_TransactionCreatedConsumer(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+	logger := getLogger()
+	topic := consumer.TopicTransactionCreated
+	viper := getViperConfig()
+	kafkaURLs := viper.GetStringSlice("kafka.urls")
+	kafkaClient := core.NewKafkaClient(kafkaURLs)
+	_, err := kafkaClient.DeleteTopics(ctx, &kafka.DeleteTopicsRequest{
+		Topics: []string{topic},
+	})
+	assert.Nil(err)
+	res, err := kafkaClient.CreateTopics(ctx, &kafka.CreateTopicsRequest{
+		Topics: []kafka.TopicConfig{
+			{
+				Topic:             topic,
+				ReplicationFactor: 1,
+				NumPartitions:     1,
+			},
+		},
+	})
+	assert.Nil(err)
+	assert.Nil(res.Errors[topic])
+	createdAt := time.Now().Add(-5 * time.Minute).Unix()
+	msgValue := transactionpb.TransactionCreatedEvent{
+		TransactionID: 1,
+		UserID:        1,
+		Amount:        100.0,
+		Balance:       150,
+		Kind:          "DEPOSIT",
+		CreatedAt:     createdAt,
+	}
+	buff, err := proto.Marshal(&msgValue)
+	assert.Nil(err)
+	kafkaWriter := core.NewKafkaWriter(kafkaURLs, "")
+	kafkaWriter.WriteMessages(ctx, kafka.Message{
+		Topic: topic,
+		Value: buff,
+		Key:   nil,
+	})
+	kafkaReaderBuilder := core.NewKafkaReaderBuilder(kafkaURLs, "", 1000)
+	kafkaReader := kafkaReaderBuilder.SetTopic(topic).Build()
+	notificationService := notification.NewService()
+	consumer := consumer.NewTransactionCreatedConsumer(kafkaReader, notificationService, logger)
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		//to be sure the data is consumed
+		time.Sleep(2 * time.Second)
+		cancel()
+	}()
+	consumer.Consume(ctx)
 
+	notifs := notificationService.GetAllNotifications()
+	assert.Len(notifs, 1)
+	n1 := notifs[0]
+	assert.Equal(n1.TransactionID, int64(1))
+	assert.Equal(n1.UserID, int64(1))
+	assert.Equal(n1.Amount, 100.0)
+	assert.Equal(n1.Balance, 150.0)
+	assert.Equal(n1.Kind, "DEPOSIT")
+	assert.Equal(n1.CreatedAt, createdAt)
 }
